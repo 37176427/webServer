63f92c1482a0c5f6264b821c68dee6497f124b21
he <37176427@qq.com>
2020-01-12 16:52:22 +0800

    修复临时目录配置

---
+++ src/main/java/com/heasy/common/config/MultipartConfig.java
@@ -0,0 +1,19 @@
+package com.heasy.common.config;
+
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+public class MultipartConfig {
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setLocation("/tmp/tomcattmp");
+		return factory.createMultipartConfig();
+	}
+}
